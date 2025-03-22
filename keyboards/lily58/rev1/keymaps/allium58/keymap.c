8a39eb3951de1c2200970cba31a4dcbe533bf686
Will Keung <williamk212@gmail.com>
2025-03-22 16:36:56 -0400

    WIP, all layers up and including _NAV

--- keyboards/lily58/rev1/keymaps/allium58/keymap.c
+++ keyboards/lily58/rev1/keymaps/allium58/keymap.c
@@ -10,15 +10,80 @@ enum layers {
     _MEDIA
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                            KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
-    ),
+#define LGUI_PLUS   LGUI_T(KC_PLUS)
+#define LALT_LPRN   LALT_T(KC_LPRN)
+#define LCTL_DQUO   LCTL_T(KC_DQUO)
+#define LSFT_RPRN   LSFT_T(KC_RPRN)
+#define RSFT_LCBR   RSFT_T(KC_LCBR)
+#define RALT_RCBR   RALT_T(KC_RCBR)
 
+// we will need to intercept mod-taps
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    // https://getreuer.info/posts/keyboards/faqs/index.html#mt-doesnt-work-with-this-keycode-qmk
+    switch (keycode) {
+        case LGUI_PLUS:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_PLUS);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+        case LALT_LPRN:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_LPRN);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+        case LCTL_DQUO:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_DQUO);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+        case LSFT_RPRN:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_RPRN);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+        case RSFT_LCBR:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_LCBR);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+        case RALT_RCBR:  // LEFT Command layer on hold, KC_PLUS on tap.
+            if (record->tap.count) {  // On tap.
+                if (record->event.pressed) {  // On press.
+                    tap_code16(KC_RCBR);
+                }
+                return false;  // Skip default handling.
+            }
+            break;
+
+    }
+    return true;  // Continue default handling.
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        // base layer; colemak DH
+        KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+        KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+        KC_ESC,   LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),   KC_G,           KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
+        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,      KC_LBRC,  KC_RBRC,          KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+
+        KC_LALT, LT(_MEDIA, KC_ESC), LT(_NAV, KC_SPC), LT(_MOUSE, KC_TAB),     LT(_SYMBOL, KC_ENT), LT(_NUMBER, KC_BSPC), LT(_SYMBOL, KC_DEL), KC_RGUI
+    ),
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -34,14 +99,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
+    [_QWERTY] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
+        _______,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        _______,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                     KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+        _______,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      _______,  _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+                    _______, _______, _______, _______,                        _______, _______, _______, _______
+    ),
 
- [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
-),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -56,11 +121,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-[_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+[_SYMBOL] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, _______,
+  _______, LGUI_T(KC_PLUS), LALT_T(KC_LPRN), LCTL_T(KC_DQUO), LSFT_T(KC_RPRN), KC_QUOTE,            KC_COLN, RSFT_T(KC_LCBR), RCTL_T(KC_EQL), RALT_T(KC_RCBR), RGUI_T(KC_SCLN), _______,
+  _______, KC_LABK, KC_PIPE, KC_MINS, KC_RABK, KC_BACKSLASH,    _______, _______,       KC_GRV, KC_UNDS, KC_QUES, KC_TILD, _______, _______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
@@ -78,12 +143,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
-[_RAISE] = LAYOUT(
+[_NUMBER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                      _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
+  _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,     _______, _______,       _______, _______, _______, KC_LALT, _______, _______,
+                    _______, _______, _______,  _______,                    _______,  _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -99,10 +164,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_ADJUST] = LAYOUT(
+  [_NAV] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
   )
@@ -149,12 +214,12 @@ bool oled_task_user(void) {
 }
 #endif // OLED_ENABLE
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-  return true;
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   if (record->event.pressed) {
+// #ifdef OLED_ENABLE
+//     set_keylog(keycode, record);
+// #endif
+//     // set_timelog();
+//   }
+//   return true;
+// }
