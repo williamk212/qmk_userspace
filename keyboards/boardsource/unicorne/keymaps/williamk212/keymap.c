e887ab1a751f81ce28fa2e108d24eb51c139b885
Will Keung <williamk212@gmail.com>
2026-03-16 13:06:42 -0400

    qwerty as default for unicorne

--- keyboards/boardsource/unicorne/keymaps/williamk212/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/williamk212/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _BASE,
-    _QWERTY,
+    _BASE = 0,
+    _COLEMAK_DH,
     _SYMBOL,
     _NUMBER,
     _NAV,
@@ -87,29 +87,43 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    // base layer; colemak DH
-	[_BASE] = LAYOUT_split_3x6_3(
-            KC_TAB,     KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                               KC_J,   KC_L,           KC_U,           KC_Y,           KC_SCLN,            KC_BSLS,
-            KC_ESC,     LGUI_T(KC_A),   LALT_T(KC_R),   LCTL_T(KC_S),   LSFT_T(KC_T),   KC_G,                               KC_M,   RSFT_T(KC_N),   RCTL_T(KC_E),   RALT_T(KC_I),   RGUI_T(KC_O),       KC_QUOT,
-            KC_LSFT,    KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                               KC_K,   KC_H,           KC_COMM,        KC_DOT,         KC_SLSH,            KC_RSFT,
-                                LT(_MEDIA, KC_ESC),     LT(_NAV, KC_SPC),     LT(_MOUSE, KC_TAB),                           LT(_SYMBOL, KC_ENT),    LT(_NUMBER, KC_BSPC),   LT(_SYMBOL, KC_DEL)
-            ),
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |ESC   |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                          | ESC  |Space | /  Tab  /       \Enter \  |BackSP|  DEL |
+ *                          |      |      |/       /         \      \ |      |      |
+ *                          `---------------------'           '------''-------------'
+ */
 
     // QWERTY LAYER
-	[_QWERTY] = LAYOUT_split_3x6_3(
-            KC_TAB,     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                               KC_Y,   KC_U,           KC_I,           KC_O,           KC_P,               KC_BSLS,
-            KC_LCTL,    LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,                               KC_H,   RSFT_T(KC_J),   RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),    KC_QUOT,
-            KC_LSFT,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                               KC_N,   KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,            KC_RSFT,
-                                                                _______, _______, _______,                                  _______, _______, _______
-            ),
+	[_BASE] = LAYOUT_split_3x6_3(
+        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
+        KC_ESC,     LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),  KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                           KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                    LT(_MEDIA, KC_ESC), LT(_NAV, KC_SPC), LT(_MOUSE, KC_TAB),       LT(_SYMBOL, KC_ENT), LT(_NUMBER, KC_BSPC), LT(_SYMBOL, KC_DEL)
+    ),
+
+    // _COLEMAK_DH layer; qwerty
+	[_COLEMAK_DH] = LAYOUT_split_3x6_3(
+        KC_TAB,     KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                               KC_J,   KC_L,           KC_U,           KC_Y,           KC_SCLN,            KC_BSLS,
+        KC_ESC,     LGUI_T(KC_A),   LALT_T(KC_R),   LCTL_T(KC_S),   LSFT_T(KC_T),   KC_G,                               KC_M,   RSFT_T(KC_N),   RCTL_T(KC_E),   RALT_T(KC_I),   RGUI_T(KC_O),       KC_QUOT,
+        KC_LSFT,    KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                               KC_K,   KC_H,           KC_COMM,        KC_DOT,         KC_SLSH,            KC_RSFT,
+                                LT(_MEDIA, KC_ESC),     LT(_NAV, KC_SPC),     LT(_MOUSE, KC_TAB),                           LT(_SYMBOL, KC_ENT),    LT(_NUMBER, KC_BSPC),   LT(_SYMBOL, KC_DEL)
+    ),
 
     // symbol layer
+    // symbol layer
 	[_SYMBOL] = LAYOUT_split_3x6_3(
-            _______,    KC_EXLM,            KC_AT,              KC_HASH,            KC_DLR,          KC_PERC,               KC_CIRC,    KC_AMPR,            KC_ASTR,            KC_LBRC,            KC_RBRC,            _______,
-            _______,    LGUI_T(KC_PLUS),    LALT_T(KC_LPRN),    LCTL_T(KC_DQUO),    LSFT_T(KC_RPRN), KC_QUOTE,              KC_COLN,    RSFT_T(KC_LCBR),    RCTL_T(KC_EQL),     RALT_T(KC_RCBR),    RGUI_T(KC_SCLN),    _______,
-            _______,    KC_LABK,            KC_PIPE,            KC_MINS,            KC_RABK,         KC_BACKSLASH,          KC_GRV,     KC_UNDS,            KC_QUES,            KC_TILD,            _______,            _______,
-                                                                                    _______, _______, _______,              _______, _______, _______
-            ),
+        _______,    KC_EXLM,    KC_AT,  KC_HASH,    KC_DLR,     KC_PERC,            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LBRC,    KC_RBRC,    _______,
+        _______,    LGUI_T(KC_PLUS), LALT_T(KC_LPRN), LCTL_T(KC_DQUO), LSFT_T(KC_RPRN), KC_QUOTE,              KC_COLN, RSFT_T(KC_LCBR), RCTL_T(KC_EQL), RALT_T(KC_RCBR), RGUI_T(KC_SCLN), _______,
+        _______,    KC_LABK,    KC_PIPE, KC_MINS,   KC_RABK, KC_BACKSLASH,          KC_GRV,     KC_UNDS,    KC_QUES,    KC_TILD,    _______,    _______,
+                                            _______, _______, _______,              _______, _______, _______
+    ),
 
 	[_NUMBER] = LAYOUT_split_3x6_3(
             _______,    KC_LBRC,        KC_7,           KC_8,           KC_9,           KC_RBRC,                            _______,    _______,    _______,        _______,        _______,            _______,
